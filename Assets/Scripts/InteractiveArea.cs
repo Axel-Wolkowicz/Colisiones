01b873b732129b83d47ec282245da44032c669de
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2026-04-30 10:38:21 -0300

    Initial commit

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+   void OnCollisionEnter(Collision col){
+    if (col.gameObject.CompareTag("pickable")){
+        Destroy(col.gameObject);
+    }
+   }
+}
